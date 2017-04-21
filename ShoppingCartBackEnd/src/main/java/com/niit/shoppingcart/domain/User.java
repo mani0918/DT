5d8e132f48bf89ec638e921486acbc390b2837ad
manideeppanchiri <manideeps.19@gmail.com>
2017-04-21 13:04:34 +0530

    Created userDao interface and the implementation classes for the interface.
    created junit test cases and solved the exceptions occured.

--- ShoppingCartBackEnd/src/main/java/com/niit/shoppingcart/domain/User.java
+++ ShoppingCartBackEnd/src/main/java/com/niit/shoppingcart/domain/User.java
@@ -1,40 +1,51 @@
 package com.niit.shoppingcart.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
 import org.springframework.stereotype.Component;
 
-//need to create singleton instance of class
-@Component
+@Entity // to map to data base table
+@Table(name = "user") // if the table name and domain class name is different
+@Component   //context.scan("com.niit") will scan the package and create instance
 public class User {
 
+	@Id
 	private String id;
+	@Column(name = "name")
 	private String name;
 	private String password;
 	private String role;
-	public String getId() {
-		return id;
-	}
+	
+
+    private int contact;
+
+
 	public void setId(String id) {
 		this.id = id;
 	}
-	public String getName() {
-		return name;
-	}
+
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	public String getPassword() {
-		return password;
-	}
+
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	public String getRole() {
-		return role;
-	}
+
+
 	public void setRole(String role) {
 		this.role = role;
 	}
-	
-	
-	
+
+
+	public void setContact(int contact) {
+		this.contact = contact;
+	}
+
 }
